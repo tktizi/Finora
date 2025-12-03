9bf4362540283850909724e6620c67c143bdea4b
tktizi <maks.krish7@gmail.com>
2025-12-04 01:53:43 +0200

    MUI Configurations and Theme

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,21 @@
+import { Box, CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme} from '@mui/material/styles';
+import { useMemo } from 'react';
+import { themeSettings } from './theme';
+import { BrowserRouter } from 'react-router-dom';
+
 function App() {
-  return <div className="app"></div>;
+  const theme = useMemo(( ) => createTheme(themeSettings), [])
+  return <div className="app">
+    <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box>
+        
+      </Box>
+    </ThemeProvider>
+    </BrowserRouter>
+  </div>;
 }
 
 export default App;
