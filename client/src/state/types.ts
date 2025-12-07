71d8012986e2d54fca4b8f6e007cac35a05d2570
tktizi <maks.krish7@gmail.com>
2025-12-07 21:57:45 +0200

    Transactions UI and API

--- client/src/state/types.ts
+++ client/src/state/types.ts
@@ -45,3 +45,13 @@ export interface GetProductsResponse {
   updatedAt: string;
 }
 
+export interface GetTransactionsResponse {
+  id: string;
+  _id: string;
+  __v: number;
+  buyer: string;
+  amount: number;
+  productIds: Array<string>;
+  createdAt: string;
+  updatedAt: string;
+}
